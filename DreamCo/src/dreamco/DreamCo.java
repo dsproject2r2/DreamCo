f46018355c30b38e3ec098edce9082884b1c6616
Jeremiah <Jeremiah@DESKTOP-P7GG928>
2019-05-11 02:36:46 +0800

    ENCRYPTION DECRYPTION READ WRITE

--- DreamCo/src/dreamco/DreamCo.java
+++ DreamCo/src/dreamco/DreamCo.java
@@ -9,7 +9,7 @@ public class DreamCo {
     public static void main(String[] args) throws IOException {
         DataFile objectDataFile = new DataFile();
         
-        DataFile.fileEncryption();
+        DataFile.fileDecryption();
     }
 
 }
