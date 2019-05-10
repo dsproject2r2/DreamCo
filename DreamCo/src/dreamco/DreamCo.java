f543da8b0fc4635677c4a345878e8198dc4ce6fd
Jeremiah <Jeremiah@DESKTOP-P7GG928>
2019-05-10 12:15:14 +0800

    ENCRYPTION DECRYPTION READ WRITE

--- DreamCo/src/dreamco/DreamCo.java
+++ DreamCo/src/dreamco/DreamCo.java
@@ -1,21 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package dreamco;
 
-/**
- *
- * @author MX-15
- */
 public class DreamCo {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
+    public static void main(String[] args) {
+        DataFile objectDataFile = new DataFile();
+        
+        objectDataFile.fileEncryption();
+    }
+
 }
