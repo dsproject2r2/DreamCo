59e5f1d4398fc400d42563c6dd5ce68b091ba384
Jeremiah <Jeremiah@DESKTOP-P7GG928>
2019-05-11 18:27:54 +0800

    Nothing

--- DreamCo/src/dreamco/DreamCo.java
+++ DreamCo/src/dreamco/DreamCo.java
@@ -9,7 +9,9 @@ public class DreamCo {
     public static void main(String[] args) throws IOException {
         DataFile objectDataFile = new DataFile();
         
-        DataFile.fileDecryption();
+        DataFile.fileEncryption();
+        DataFile.appendFile();
+
     }
 
 }
