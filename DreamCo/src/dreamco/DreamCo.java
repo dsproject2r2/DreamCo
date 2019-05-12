4a8eb939ee50e7260058cc8cd8357a7c9da37cb2
Jeremiah <Jeremiah@DESKTOP-P7GG928>
2019-05-12 15:30:34 +0800

    Ignore this. just a tester to test out new classes

--- DreamCo/src/dreamco/DreamCo.java
+++ DreamCo/src/dreamco/DreamCo.java
@@ -2,16 +2,21 @@
 package dreamco;
 
 import java.io.IOException;
+import javax.swing.JFrame;
 
 public class DreamCo {
 
- /////////////////////////////////////////////////////////////////////////////////////This Class Serves as a tester class for now!   
+ /////////////////////////////////////////////////////////////////////////////////////    This Class Serves as a tester class for now!   ////////////////////////////////////////
 public static void main(String[] args) throws IOException {
+    //////////////////////////////////////////////////////////////////These are the objects to use other classes in the package...............
     DataEncryptionFile objectDataFile = new DataEncryptionFile();
-        
-    DataEncryptionFile.fileEncryption();
-    DataEncryptionFile.fileDecryption();
-    DataEncryptionFile.appendFile();    
+    Gui startupGui= new Gui();
+    
+    //programGui.dialogBox();    
+    startupGui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    startupGui.setSize(400,700);
+    startupGui.setLocationRelativeTo(null);
+    startupGui.setVisible(true);
     }
 
 }
