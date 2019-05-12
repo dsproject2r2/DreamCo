b5bdf636cc27459f441a35b450ac76e298d2ef74
Jeremiah <Jeremiah@DESKTOP-P7GG928>
2019-05-12 16:19:56 +0800

    =

--- DreamCo/src/dreamco/Gui.java
+++ DreamCo/src/dreamco/Gui.java
@@ -10,10 +10,15 @@ import javax.swing.JTextField;
 import java.awt.FlowLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+//imports for the buttons
+import javax.swing.JButton;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 
 
 public class Gui extends JFrame{
 
+//////////////////////////////////////////////////////////////////////////////////////////////////
 public static void dialogBox(){    
     String fn= JOptionPane.showInputDialog("Enter Name");
     String sn= JOptionPane.showInputDialog("Enter Password");    
@@ -30,10 +35,18 @@ private JTextField fld2;
 private JTextField fld3;
 private JPasswordField pass;
 
+private JButton yesbutton;
+private JButton nobutton;
+private JButton custombutton;
+
 ///////////////////////////////// /////////////////////////////////////////////////////////////This is just a constructor for the STARTUP Gui Frame/////////////////////////////////
 public Gui(){
     super("2R2 DreamCo Scam"); //The 
     setLayout(new FlowLayout());
+    
+    Icon b= new ImageIcon(getClass().getResource("b.png"));
+    custombutton=new JButton("Custom",b);
+    custombutton.setRolloverIcon(b);
     
     item1=new JLabel("This is a sentence");
     item1.setToolTipText("This is going to show up on hover");
@@ -52,6 +65,12 @@ public Gui(){
     pass=new JPasswordField(10);
     add(pass);
     
+    yesbutton= new JButton("Yes");
+    add(yesbutton);
+    nobutton=new JButton("no");
+    add(nobutton);
+    
+    
     ActHandler handler= new ActHandler();
     fld1.addActionListener(handler);
     fld2.addActionListener(handler);
@@ -59,22 +78,25 @@ public Gui(){
     pass.addActionListener(handler);
 }
 
-private class ActHandler implements ActionListener{  
+private class ActHandler implements ActionListener{  //This class is a subclass for action listener to handle events 
     @Override
     public void actionPerformed(ActionEvent event){
         String string= "";
 
         if(event.getSource()==fld1)
             string=String.format("field 1: %s", event.getActionCommand());
-        if(event.getSource()==fld2)
+        else if(event.getSource()==fld2)
             string=String.format("field 1: %s", event.getActionCommand());
-        if(event.getSource()==fld3)
+        else if(event.getSource()==fld3)
             string=String.format("field 1: %s", event.getActionCommand());  
-        if(event.getSource()==pass)
-            string=String.format("password is: %s", event.getActionCommand());        
+        else if(event.getSource()==pass)
+            string=String.format("password is: %s", event.getActionCommand());      
+        
+        JOptionPane.showMessageDialog(null, string);
     }  
 }
 /////////////////////////////////////////////////////////////////////////////////////////////// THIS IS WHERE STARTUP Gui Frame ENDS //////////////////////////////////////////////////////////
+
 
 
 }
