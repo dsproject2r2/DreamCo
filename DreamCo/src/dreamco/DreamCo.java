35541e36a94d4a528f1aee29e2c2d1cc28cbf7f4
Jeremiah <Jeremiah@DESKTOP-P7GG928>
2019-05-13 01:14:12 +0800

    no changes still a tester  class

--- DreamCo/src/dreamco/DreamCo.java
+++ DreamCo/src/dreamco/DreamCo.java
@@ -10,11 +10,15 @@ public class DreamCo {
     
  /////////////////////////////////////////////////////////////////////////////////////    This Class Serves as a tester class for now!   ////////////////////////////////////////
 public static void main(String[] args) throws IOException {
-    //////////////////////////////////////////////////////////////////These are the objects to use other classes in the package...............
+    ///////////////////////////////////////////These are the objects to use other classes in the package...............
     DataEncryptionFile objectDataFile = new DataEncryptionFile();
     Gui startupGui= new Gui();
+    SplashScreen splash=new SplashScreen();
+    Log login=new Log();
+
+    login.runLoginFrame();
     
-    Gui.runStartupFrame();
+    
 
     }
 
