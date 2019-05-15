fea53354b849ad69218c01303d4e729e5a0f1e31
MX-15 <MX-15@CatchMeIfYouCan>
2019-05-15 16:29:10 +0800

    Fix bugs

--- DreamCo/src/dreamco/UserFrame.java
+++ DreamCo/src/dreamco/UserFrame.java
@@ -27,6 +27,8 @@ public class UserFrame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTree1 = new javax.swing.JTree();
         jPanel1 = new javax.swing.JPanel();
         jButton1 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
@@ -36,6 +38,12 @@ public class UserFrame extends javax.swing.JFrame {
         jLabel5 = new javax.swing.JLabel();
         jTextField2 = new javax.swing.JTextField();
         jTextField3 = new javax.swing.JTextField();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
+
+        jScrollPane1.setViewportView(jTree1);
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setResizable(false);
@@ -45,7 +53,7 @@ public class UserFrame extends javax.swing.JFrame {
         jPanel1.setPreferredSize(new java.awt.Dimension(450, 600));
         jPanel1.setLayout(null);
 
-        jButton1.setBackground(new java.awt.Color(102, 102, 102));
+        jButton1.setBackground(new java.awt.Color(255, 255, 255));
         jButton1.setForeground(new java.awt.Color(204, 204, 204));
         jButton1.setText("Register User");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -57,24 +65,24 @@ public class UserFrame extends javax.swing.JFrame {
         jButton1.setBounds(60, 400, 330, 50);
 
         jLabel1.setFont(new java.awt.Font("SansSerif", 1, 22)); // NOI18N
-        jLabel1.setText("User  ID");
+        jLabel1.setText("User  ID:");
         jPanel1.add(jLabel1);
-        jLabel1.setBounds(60, 500, 90, 29);
+        jLabel1.setBounds(70, 500, 90, 29);
 
-        jLabel2.setFont(new java.awt.Font("Myriad Pro", 1, 22)); // NOI18N
-        jLabel2.setText("Revenue");
+        jLabel2.setFont(new java.awt.Font("SansSerif", 1, 22)); // NOI18N
+        jLabel2.setText("Revenue:");
         jPanel1.add(jLabel2);
-        jLabel2.setBounds(60, 540, 140, 23);
+        jLabel2.setBounds(60, 540, 100, 29);
 
         jLabel3.setFont(new java.awt.Font("Myriad Pro", 1, 22)); // NOI18N
         jLabel3.setText("userIDvariable");
         jPanel1.add(jLabel3);
-        jLabel3.setBounds(180, 510, 170, 23);
+        jLabel3.setBounds(170, 500, 170, 30);
 
         jLabel4.setFont(new java.awt.Font("Myriad Pro", 1, 22)); // NOI18N
         jLabel4.setText("userRevenueVariable");
         jPanel1.add(jLabel4);
-        jLabel4.setBounds(180, 540, 220, 23);
+        jLabel4.setBounds(170, 540, 220, 23);
 
         jLabel5.setFont(new java.awt.Font("SansSerif", 1, 36)); // NOI18N
         jLabel5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
@@ -83,13 +91,44 @@ public class UserFrame extends javax.swing.JFrame {
         jPanel1.add(jLabel5);
         jLabel5.setBounds(30, 10, 400, 150);
 
-        jTextField2.setText("jTextField2");
+        jTextField2.setBackground(new java.awt.Color(255, 255, 255));
+        jTextField2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField2ActionPerformed(evt);
+            }
+        });
         jPanel1.add(jTextField2);
-        jTextField2.setBounds(170, 290, 260, 50);
+        jTextField2.setBounds(170, 320, 260, 40);
 
-        jTextField3.setText("jTextField2");
+        jTextField3.setBackground(new java.awt.Color(255, 255, 255));
         jPanel1.add(jTextField3);
-        jTextField3.setBounds(170, 220, 260, 50);
+        jTextField3.setBounds(170, 260, 260, 40);
+
+        jLabel6.setBackground(new java.awt.Color(102, 102, 102));
+        jLabel6.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+        jLabel6.setText("New Username");
+        jPanel1.add(jLabel6);
+        jLabel6.setBounds(20, 260, 150, 40);
+
+        jLabel7.setBackground(new java.awt.Color(102, 102, 102));
+        jLabel7.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+        jLabel7.setText("New Password");
+        jPanel1.add(jLabel7);
+        jLabel7.setBounds(20, 320, 150, 40);
+
+        jLabel8.setBackground(new java.awt.Color(0, 0, 0));
+        jLabel8.setFont(new java.awt.Font("Myriad Pro", 1, 18)); // NOI18N
+        jLabel8.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel8.setText("It's All About Our Perception! Build Your Empire.");
+        jPanel1.add(jLabel8);
+        jLabel8.setBounds(0, 160, 450, 30);
+
+        jLabel9.setFont(new java.awt.Font("Courier New", 1, 14)); // NOI18N
+        jLabel9.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel9.setText("Register your customer here");
+        jLabel9.setToolTipText("");
+        jPanel1.add(jLabel9);
+        jLabel9.setBounds(0, 200, 450, 20);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -110,6 +149,10 @@ public class UserFrame extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
 
+    private void jTextField2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField2ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -118,23 +161,7 @@ public class UserFrame extends javax.swing.JFrame {
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(AdminFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(AdminFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(AdminFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(AdminFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
+         */       
         //</editor-fold>
 
         /* Create and display the form */
@@ -152,8 +179,14 @@ public class UserFrame extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextField jTextField2;
     private javax.swing.JTextField jTextField3;
+    private javax.swing.JTree jTree1;
     // End of variables declaration//GEN-END:variables
 }
