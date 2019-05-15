6c24aa19014fd79caa6680f060e492ed8b925505
Jeremiah <Jeremiah@DESKTOP-P7GG928>
2019-05-15 16:44:55 +0800

    Code cleanup and class renaming

--- DreamCo/src/dreamco/LoginFrame.java
+++ DreamCo/src/dreamco/LoginFrame.java
@@ -222,6 +222,7 @@ public class LoginFrame extends javax.swing.JFrame {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new LoginFrame().setVisible(true);
+                
             }
         });
     }
