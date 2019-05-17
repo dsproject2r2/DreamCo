969cb096484db727dac33df5d5c9197e1dad1a6c
Jeremiah <Jeremiah@DESKTOP-P7GG928>
2019-05-18 03:36:23 +0800

    Method to change registration fee

--- DreamCo/src/dreamco/AdminFrame.java
+++ DreamCo/src/dreamco/AdminFrame.java
@@ -163,13 +163,26 @@ public class AdminFrame extends javax.swing.JFrame {
             }
         });
     }
-    
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////// METHOD TO CHANGE REGISTRATION FEE ///////////////////////////////////////    
     public static double changeRegistrationFee(){   
     String fee= JOptionPane.showInputDialog("Enter new Registration Fee");
-//   feereturn =Integer.parseInt(fee);
-    return feereturn;
+    double feereturn;
+    if(fee!=null){
+        feereturn =Integer.parseInt(fee);        
+        JOptionPane.showMessageDialog(null, "New Registration Fee of RM"+ feereturn+ " has been set!" , "Success!", JOptionPane.PLAIN_MESSAGE); //shows tat a message box
     }
-
+    else{
+        feereturn=mytree.getFee();
+    }
+        return feereturn;
+    }
+////////////////////////////////////////////////////////////////////////////////////////////////////////// 
+    
+    
+    
+    
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
