55732556391a26fae77c6bd3c920f7d4f6ba5e59
Jeremiah <Jeremiah@DESKTOP-P7GG928>
2019-05-17 23:11:24 +0800

    no changes

--- DreamCo/src/dreamco/DreamCo.java
+++ DreamCo/src/dreamco/DreamCo.java
@@ -14,17 +14,24 @@ public static void main(String[] args) throws IOException {
     ///////////////////////////////////////////These are the OBJECTS to use OTHER classes in the package...............
     DataEncryptionFile objectDataFile = new DataEncryptionFile();
     MyTree tree= new MyTree();
-
+    SplashScreen splashscreen= new SplashScreen();
+    AdminFrame adminframe= new AdminFrame();
     
-    Scanner s=new Scanner(System.in);
-    System.out.println("enter name");
-    String name=s.nextLine();
-    String password=s.nextLine();
-    String parents =s.nextLine();
-    tree.add(name, password,parents);
-  
-    Login login = new Login();
-    login.runLoginFrame();
+    System.out.println(tree.getFee());
+    adminframe.changeRegistrationFee();
+    System.out.println(tree.getFee());
+//    splashscreen.startupSplashScreen();
+    
+    
+//    Scanner s=new Scanner(System.in);
+//    System.out.println("enter name");
+//    String name=s.nextLine();
+//    String password=s.nextLine();
+//    String parents =s.nextLine();
+//    tree.add(name, password,parents);
+//  
+//    LoginFrame login = new LoginFrame();
+//    login.runLoginFrame();
     
 
     }
